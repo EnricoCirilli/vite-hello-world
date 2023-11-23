7cb1c348bee0f2e24bc05f835fd0a4bc2f6386a3
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-23 14:03:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="">
+
+  <AppHeader />
+
+  <h1>Component APP</h1>
+  
+</template>
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <h1 class="text-danger">{{ text }}</h1>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            text: 'Header'
+        }
+    }
+}
+</script>
+<style scoped>
+    h1 {
+        color: seagreen;
+    }
+</style>
